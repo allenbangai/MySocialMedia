9b7bc706d47aee655b6106569527b8ae4b88eb0a
=allenbangai <=allenkamadje@gmail.com>
2019-09-13 23:51:19 +0100

    Authenticating and Creating user account

--- app/src/main/java/com/example/mysocialmedia/RegisterActivity.java
+++ app/src/main/java/com/example/mysocialmedia/RegisterActivity.java
@@ -1,25 +1,113 @@
 package com.example.mysocialmedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    EditText registerUserEmail, registerUserPassword, registerUserConfirmPassword;
-    Button registerCreateAccountButton;
+    private EditText registerUserEmail, registerUserPassword, registerUserConfirmPassword;
+    private Button registerCreateAccountButton;
+    private ProgressDialog loadingbar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
+
         registerCreateAccountButton = findViewById(R.id.id_button_register);
         registerUserConfirmPassword = findViewById(R.id.id_register_confirm_password);
         registerUserPassword = findViewById(R.id.id_register_password);
         registerUserEmail = findViewById(R.id.id_register_email);
+
+        registerCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createAccout();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void sendUserToMainActivity() {
+        Intent mainActivityIntent = new Intent(RegisterActivity.this, MainActivity.class);
+        mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainActivityIntent);
+    }
+
+    private void createAccout() {
+        String email = registerUserEmail.getText().toString();
+        String password = registerUserPassword.getText().toString();
+        String confirmPassword = registerUserConfirmPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "enter email, Email Address field is empty", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(email)){
+            Toast.makeText(this, "Password fiels is empty", Toast.LENGTH_LONG).show();
+        }
+        else if(TextUtils.isEmpty(email)){
+            Toast.makeText(this,"Confirm Password field is empty", Toast.LENGTH_SHORT).show();
+        }
+        else if(password.length()<6){
+            Toast.makeText(this, "Password length should be greater than 5", Toast.LENGTH_LONG).show();
+        }
+        else if(!confirmPassword.equals(password)){
+            Toast.makeText(this, "Password does not match Confirm Password", Toast.LENGTH_LONG).show();
+        }
+        else{
+            loadingbar = new ProgressDialog(this);
+            loadingbar.setTitle("Creating User Account");
+            loadingbar.setMessage("Please wait while we are creating your new account");
+            loadingbar.show();
+            loadingbar.setCanceledOnTouchOutside(true);
+
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        sendUserToSetupActivity();
+
+                        Toast.makeText(RegisterActivity.this,"You have being authenticated", Toast.LENGTH_SHORT).show();
+                        loadingbar.dismiss();
+                    }
+                    else{
+                        String message = task.getException().getMessage();
+                        Toast.makeText(RegisterActivity.this, "Error occured \n"+message, Toast.LENGTH_LONG).show();
+                        loadingbar.dismiss();
+                    }
+                }
+            });
+        }
+    }
+
+    private void sendUserToSetupActivity() {
+        Intent setupActivityIntent = new Intent(RegisterActivity.this, SetupActivity.class);
+        setupActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(setupActivityIntent);
+        finish();
     }
 
 
--- app/src/main/java/com/example/mysocialmedia/SetupActivity.java
+++ app/src/main/java/com/example/mysocialmedia/SetupActivity.java
@@ -1,10 +1,27 @@
 package com.example.mysocialmedia;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -13,16 +30,105 @@ public class SetupActivity extends AppCompatActivity {
     private EditText setupUsername, setupFullName, setupCountryName;
     private Button setupSaveInfo;
     private CircleImageView setupProfieImage;
+    private ProgressDialog loadingbar;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mUserReference;
+
+
+    String currentUserId;
+    final static int gallery_Prick = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getCurrentUser().getUid();
+        mUserReference = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUserId);
+
         setupCountryName = findViewById(R.id.id_setup_country);
         setupFullName = findViewById(R.id.id_setuo_full_name);
         setupProfieImage = findViewById(R.id.id_setup_profile_image);
         setupUsername = findViewById(R.id.id_setup_username);
         setupSaveInfo = findViewById(R.id.id_setup_save_button);
+
+        setupSaveInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveSetupAccountInfo();
+            }
+        });
+
+        setupProfieImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent();
+                galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, gallery_Prick);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == gallery_Prick && resultCode == RESULT_OK && data != null){
+            Uri ImageUri = data.getData();
+        }
+    }
+
+    private void saveSetupAccountInfo() {
+        String fullName = setupFullName.getText().toString();
+        String userName = setupUsername.getText().toString();
+        String country = setupCountryName.getText().toString();
+
+        if(TextUtils.isEmpty(fullName)){
+            Toast.makeText(this, "Full Name field is empty", Toast.LENGTH_LONG).show();
+        }
+        else if(TextUtils.isEmpty(userName)){
+            Toast.makeText(this, "User Name field is empty", Toast.LENGTH_LONG).show();
+        }
+        else if(TextUtils.isEmpty(country)){
+            Toast.makeText(this, "Country field is empty", Toast.LENGTH_LONG).show();
+        }
+        else{
+            HashMap userMap = new HashMap();
+            userMap.put("username", userName);
+            userMap.put("full_name", fullName);
+            userMap.put("country", country);
+            userMap.put("status", "Hey, using my social media");
+            userMap.put("level_of_Study", "none");
+            userMap.put("gender", "none");
+            userMap.put("birth_date", "none");
+
+
+            loadingbar = new ProgressDialog(this);
+            loadingbar.setTitle("Saving Info");
+            loadingbar.setMessage("Creating Your New Account");
+            loadingbar.show();
+            loadingbar.setCanceledOnTouchOutside(true);
+
+            mUserReference.updateChildren(userMap).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if(task.isSuccessful()){
+                        sendUserToMainActivity();
+                        Toast.makeText(SetupActivity.this, "Your account is created successfully", Toast.LENGTH_LONG);
+                        loadingbar.dismiss();
+                    }else{
+                        String message = task.getException().getMessage();
+                        Toast.makeText(SetupActivity.this, "Error occured \n"+ message, Toast.LENGTH_LONG);
+                        loadingbar.dismiss();
+                    }
+                }
+            });
+        }
+    }
+    private void sendUserToMainActivity() {
+        Intent mainActivityIntent = new Intent(SetupActivity.this, MainActivity.class);
+        mainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK| Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainActivityIntent);
     }
 }
